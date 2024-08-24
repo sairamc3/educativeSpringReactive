1390c433e9535e6cfb2628f2d6e9bad1ab7f5211
Sairam Chekuri <sairamc3@gmail.com>
2024-08-24 20:43:32 +0530

    initial commit

---
+++ hackingspringbootreactive/src/main/java/com/greglturnquist/hackingspringbootreactive/HackingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingWithSpringBootApplication.class, args);
+	}
+
+}
